d6c5578cf6cebb96b58627efc482baa7a92886c0
Govind Bajaj <g.bajaj@livello.com>
2025-12-26 22:05:10 +0530

    updates

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Montserrat } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 import { getSiteSettings } from "@/lib/siteMetadata";
 
@@ -75,18 +76,21 @@ export default function RootLayout({
   };
   return (
     <html lang="en">
-      <head>
-        {/* Google Tag Manager - Head Script */}
+      <body className={`${montserrat.variable} ${montserratMono.variable}`}>
+        {/*
+          In the App Router, Next manages the document <head>.
+          Using next/script ensures the GTM loader is inserted correctly (head/body as appropriate)
+          and avoids accidental <head>/<meta> output inside <body>.
+        */}
         {GTM_ID && (
-          <script
-            id="gtm-head"
+          <Script
+            id="gtm"
+            strategy="beforeInteractive"
             dangerouslySetInnerHTML={{
               __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','${GTM_ID}');`,
             }}
           />
         )}
-      </head>
-      <body className={`${montserrat.variable} ${montserratMono.variable}`}>
         {/* Google Tag Manager - Noscript (immediately after body) */}
         {GTM_ID && (
           <noscript
